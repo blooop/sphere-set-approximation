b48cc90c09adc9d3b07ed5120ebb20b72e3abfdf
Austin Gregg-Smith <blooop@gmail.com>
2024-12-13 22:24:00 +0000

    update main to take number of spheres

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include "sphere_set_approximator.hpp"
+#include <iostream>
+#include <cstdlib>
 
 int main(int argc, const char* argv[])
 {
+    //Ensure the program is called with the correct number of arguments
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <input_file> <output_file> <number_of_spheres>" << std::endl;
+        return 1;
+    }
+
+    // Parse the number of spheres
+    char* end;
+    int n_sphere = std::strtol(argv[3], &end, 10);
+    if (*end != '\0' || n_sphere <= 0) {
+        std::cerr << "Error: <number_of_spheres> must be a positive integer." << std::endl;
+        return 1;
+    }
 	sphere_set_approximator::sphere_set_approximator_params params;
 	// parse command line arguments
 	params.resolution = 20000;
@@ -13,7 +28,7 @@ int main(int argc, const char* argv[])
 	params.n_finalsample = 100000;
 	params.n_innersample = 8000;
 	params.n_mutate = 10;
-	params.n_sphere = 64;
+	params.n_sphere = n_sphere;
 	params.n_surfacesample = 4000;
 	params.seed = 19260817;
 
