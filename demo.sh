50001bc45038614640f1980cd21cdcc257f18113
Austin Gregg-Smith <blooop@gmail.com>
2024-12-13 22:24:15 +0000

    update demo to take number of spheres

--- demo.sh
+++ demo.sh
@@ -4,5 +4,5 @@ mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
-./main ../example/armadillo.obj ../example/armadillo.json
+./main ../example/armadillo.obj ../example/armadillo.json 10
 
